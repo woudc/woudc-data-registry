8673c6b5df29befef3737a1ff4ee4bd46b55f760
danielwaiforssell <51061073+danielwaiforssell@users.noreply.github.com>
2020-12-17 13:06:18 -0500

    Changed variable name stringType to is_string

--- woudc_data_registry/dataset_validators.py
+++ woudc_data_registry/dataset_validators.py
@@ -180,7 +180,7 @@ class TotalOzoneValidator(DatasetValidator):
 
         daily_columns = zip(*extcsv.extcsv['DAILY'].values())
 
-        stringType = False
+        is_string = False
 
         in_order = True
         prev_date = None
@@ -209,12 +209,12 @@ class TotalOzoneValidator(DatasetValidator):
         dateList = extcsv.extcsv['DAILY']['Date']
         for date in dateList:
             if isinstance(date, (str, int)):
-                stringType = True
+                is_string = True
                 if not self._add_to_report(102, daily_startline):
                     success = False
                 break
 
-        if not stringType:
+        if not is_string:
             for index in rows_to_remove:
                 for column in extcsv.extcsv['DAILY'].values():
                     column.pop(index)
