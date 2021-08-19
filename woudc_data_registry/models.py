3376052c761680b6f3149e9ecf42ff10d717a755
victoriarspada <victoria.spada@canada.ca>
2021-08-19 10:20:48 -0400

    Update PeerDataRecords Model
    Update PeerDataRecords model to allow station types landFixed and lanOnIce

--- woudc_data_registry/models.py
+++ woudc_data_registry/models.py
@@ -1160,7 +1160,7 @@ class PeerDataRecord(base):
     station_name_id = Column(String,
                              ForeignKey('station_names.station_name_id'),
                              nullable=False)
-    stn_type_enum = Enum('land', name='stn_type')
+    stn_type_enum = Enum('land', 'landFixed', 'landOnIce', name='stn_type')
 
     station_type = Column(stn_type_enum, nullable=False, default='land')
     gaw_id = Column(String, nullable=True)
